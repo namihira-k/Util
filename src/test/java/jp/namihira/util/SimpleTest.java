ef4268b64ed4ebd8fc08182de469ba5d97dad4cf
namihira <namihira@gmail.com>
2014-08-13 12:17:17 +0900

    add SimpleTest class

---
+++ src/test/java/jp/namihira/util/SimpleTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2014 Kosuke Namihira All Rights Reserved.
+ */
+
+package jp.namihira.util;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.junit.Test;
+
+/**
+ * class for just test.
+ */
+public class SimpleTest {
+
+	/**
+	 * for test Arrays.sorts
+	 */
+	@Test
+	public void testArraysSort(){
+		//prepare
+		String[] strs = new String[]{"a", "ccc", "bb"};
+
+		//action
+		Arrays.sort(strs, new LengthComparator());
+
+		//check
+		assertEquals("a", strs[0]);
+		assertEquals("bb", strs[1]);
+		assertEquals("cc", strs[2]);
+	}
+
+	class LengthComparator implements Comparator<String> {
+		public int compare(String first, String second) {
+			return Integer.compare(first.length(), second.length());
+		}
+	}
+
+}
